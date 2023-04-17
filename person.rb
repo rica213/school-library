374d72b89244a5de998cc80fdb7281112b9437ea
Tao (Larissa) <25f5k252l@mozmail.com>
2023-04-17 22:12:29 +0300

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,5 +1,5 @@
 class Person
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..2000)
     @name = name
     @age = age
@@ -10,7 +10,7 @@ class Person
   attr_accessor :name, :age
 
   def can_use_services?
-     of_age? || @parent_permission == true
+    of_age? || @parent_permission == true
   end
 
   private
