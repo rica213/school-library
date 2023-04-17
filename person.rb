a7b18bf63ba82008c0a40537b7065651b74aed5d
rica213 <25f5k252l@mozmail.com>
2023-04-17 18:43:13 +0300

    Create the class Person

---
+++ person.rb
@@ -0,0 +1,23 @@
+class Person
+  def initialize(age, name = 'Unknown', parent_permission = 'true')
+    @id = Random.rand(1..2000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def can_use_services?
+    return unless of_age? || @parent_permission == true
+
+    true
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
