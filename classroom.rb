15391d3f6e40b3a769b9ddcc2b40263ecd407a9d
rica213 <25f5k252l@mozmail.com>
2023-04-19 16:06:00 +0300

    Establish a relationship between classroom and student

---
+++ classroom.rb
@@ -0,0 +1,16 @@
+require './student'
+
+class Classroom
+  attr_accessor :label
+  attr_reader :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def students=(student)
+    @students.append(student)
+    student.classroom = self
+  end
+end
