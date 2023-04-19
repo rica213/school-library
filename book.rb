df447c4aca674bbe1eeed63489386ac6394f1b75
rica213 <25f5k252l@mozmail.com>
2023-04-19 17:55:12 +0300

    Implement required changes

--- book.rb
+++ book.rb
@@ -1,8 +1,7 @@
 require_relative './rental'
 
 class Book
-  attr_accessor :title, :author
-  attr_reader :rentals
+  attr_accessor :title, :author, :rentals
 
   def initialize(title, author)
     @title = title
@@ -10,7 +9,7 @@ class Book
     @rentals = []
   end
 
-  def rentals=(rental)
-    @rentals.append(rental)
+  def add_rental(person, date)
+    Rental.new(date, self, person)
   end
 end
